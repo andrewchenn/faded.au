1d0171684244ab6e9ed8fa4534239f2507438448
Andrew Chen <andrewnehc11@gmail.com>
2024-07-23 17:02:59 +1000

    fix scroll on navbar

--- sidebar.js
+++ sidebar.js
@@ -1,12 +1,16 @@
 function showSidebar(){
     const sidebar = document.querySelector('.mobileSidebar');
+    const body = document.querySelector('body')
     const menuButton = document.querySelector('.fa-bars')
     sidebar.style.left = '0'
     menuButton.style.color = 'black'
+    body.style.overflowY = 'hidden'
 }
 function closeSidebar(){
     const sidebar = document.querySelector('.mobileSidebar');
     const closeButton = document.querySelector('.fa-bars')
+    const body = document.querySelector('body')
     sidebar.style.left = '-100%'
     closeButton.style.color = 'white'
+    body.style.overflowY = 'auto'
 }
