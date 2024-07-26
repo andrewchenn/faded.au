bceec80f00e7aa78179021349ddd8e66bb67c18b
unknown <andrewnehc11@gmail.comgit config --global user.name 'andrewchenn>
2024-07-26 23:54:38 +1000

    add stuff?

--- script.js
+++ script.js
@@ -16,12 +16,15 @@ function closeSidebar(){
 }
 function stopWhitespace(){
     const menu = document.querySelector('.menu')
-    if (window.screenY>5) {
-        console.log(window.screenY)
+    if (window.scrollY > 5){
         menu.style.position = 'fixed'
     }
-}
-stopWhitespace()
+    else{
+        menu.style.position = 'relative'
+    }
+};
+window.addEventListener('scroll', stopWhitespace);
+
 function discoverScroll() {
     closeSidebar()
     window.scroll({
@@ -29,3 +32,43 @@ function discoverScroll() {
         behavior: 'smooth',
     })
   }
+
+// set up text to print, each item in array is new line
+var aText = new Array(
+    "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ex minus eveniet, fuga dignissimos nisi qui perferendis, ullam eaque aperiam incidunt tenetur asperiores non repellat. Fugit ullam molestias magnam cum voluptatem."
+    );
+    var iSpeed = 500; // time delay of print out
+    var iIndex = 0; // start printing array at this posision
+    var iArrLength = aText[0].length; // the length of the text array
+    var iScrollAt = 20; // start scrolling up at this many lines
+     
+    var iTextPos = 0; // initialise text position
+    var sContents = ''; // initialise contents variable
+    var iRow; // initialise current row
+     
+    function typewriter()
+    {
+        if (window.scrollY > 460){
+            sContents =  ' ';
+            iRow = Math.max(0, iIndex-iScrollAt);
+            var destination = document.getElementById("typedtext");
+            
+            while ( iRow < iIndex ) {
+            sContents += aText[iRow++] + '<br />';
+            }
+            destination.innerHTML = sContents + aText[iIndex].substring(0, iTextPos) + "_";
+            if ( iTextPos++ == iArrLength ) {
+            iTextPos = 0;
+            iIndex++;
+            if ( iIndex != aText.length ) {
+            iArrLength = aText[iIndex].length;
+            setTimeout("typewriter()", 500);
+            }
+            } else {
+            setTimeout("typewriter()", iSpeed);
+            }
+        }
+    }
+    
+    window.addEventListener('scroll', typewriter);
+
