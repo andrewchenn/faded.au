3254ce8c61d0b5b9d7422730530fe3963dff499f
unknown <andrewnehc11@gmail.comgit config --global user.name 'andrewchenn>
2024-09-04 23:07:35 +1000

    big update, add testimonials

--- script.js
+++ script.js
@@ -39,8 +39,7 @@ function stopWhitespace(){
     }
     else{
         menu.style.position = 'relative'
-        menu.style.background = 'rgba(0,0,0,0.3)'
-        menu.style.backdropFilter = "blur(5.5px)"
+        menu.style.background = 'rgba(0,0,0,0.0)'
     }
 };
 window.addEventListener('scroll', stopWhitespace);
@@ -53,42 +52,4 @@ function menuScroll(scroll) {
     })
   }
 
-// set up text to print, each item in array is new line
-var aText = new Array(
-    "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ex minus eveniet, fuga dignissimos nisi qui perferendis, ullam eaque aperiam incidunt tenetur asperiores non repellat."
-    );
-    var iSpeed = 500; // time delay of print out
-    var iIndex = 0; // start printing array at this posision
-    var iArrLength = aText[0].length; // the length of the text array
-    var iScrollAt = 20; // start scrolling up at this many lines
-     
-    var iTextPos = 0; // initialise text position
-    var sContents = ''; // initialise contents variable
-    var iRow; // initialise current row
-     
-    function typewriter()
-    {
-        if (window.scrollY > 460){
-            sContents =  ' ';
-            iRow = Math.max(0, iIndex-iScrollAt);
-            var destination = document.getElementById("typedtext");
-            
-            while ( iRow < iIndex ) {
-            sContents += aText[iRow++] + '<br />';
-            }
-            destination.innerHTML = sContents + aText[iIndex].substring(0, iTextPos) + "_";
-            if ( iTextPos++ == iArrLength ) {
-            iTextPos = 0;
-            iIndex++;
-            if ( iIndex != aText.length ) {
-            iArrLength = aText[iIndex].length;
-            setTimeout("typewriter()", 500);
-            }
-            } else {
-            setTimeout("typewriter()", iSpeed);
-            }
-        }
-    }
-    
-    window.addEventListener('scroll', typewriter);
 
